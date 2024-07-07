9cad925b49c3ab8869465744ef75063ff958ee25
Hossein Ganji <h.ganji2233@gmail.com>
2024-07-07 21:58:42 +0330

    login and signup added

---
+++ src/components/authentication/login.js
@@ -0,0 +1,210 @@
+import { useState, useEffect } from "react";
+import "../../styles/authentication/login.css";
+import { Link } from "react-router-dom";
+import { api } from "../../vars/JwtToken.js";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("jwtAccessToken")) {
+      navigate("/");
+    }
+  }, []);
+
+  const handleUsernameChange = (event) => {
+    setUsername("");
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+
+    const registerUser = async (username, password) => {
+      const data = {
+        username: username,
+        password: password,
+      };
+
+      try {
+        const response = await api.post("/auth/jwt/create/", data, {
+          headers: { "Content-Type": "application/json" },
+        });
+        const token = response.data.access;
+        localStorage.setItem("jwtAccessToken", token);
+        if (response.status === 200) {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
+          Toast.fire({
+            icon: "success",
+            title: "ورود با موفقیت انجام شد",
+          });
+          navigate("/");
+          const responseJobFinder = await api.post("/api/profiles/", {}, {
+            headers: { "Content-Type": "application/json", Authorization: 'JWT ' + token },
+          });
+          try{
+            console.log("try");
+            navigate("/");
+          }catch(error){
+            console.log("error");
+            navigate("/");
+          }
+        }
+      } catch (error) {
+        if (error) {
+          console.log(error);
+          if (error) {
+            if (error.response.data.username) {
+              if (
+                error.response.data.username[0] === "This field may not be blank."
+              ) {
+                setUsername("فیلد ایمیل الزامی است");
+              }
+            }
+            if (error.response.data.password) {
+              if (
+                error.response.data.password[0] ===
+                "This field may not be blank."
+              ) {
+                setPassword("فیلد رمز عبور الزامی است");
+              }
+            }
+            if (error.response.data.detail) {
+              if (
+                error.response.data.detail ===
+                "No active account found with the given credentials"
+              ) {
+                setPassword("ایمیل یا رمز عبور نادرست است");
+              }
+            }
+            return;
+          }
+        }
+      }
+    };
+
+    registerUser(username, password);
+  };
+
+  return (
+    <div className="login-container">
+      <img
+        className="login-background-img"
+        src={process.env.PUBLIC_URL + "/picturs/auth/login/background.png"}
+        alt=""
+        style={{ position: "absolute", top: "0px" }}
+      />
+      <div className="login-box py-3">
+        <div
+          className="text-white h1 text-end mx-4 mt-1"
+          style={{ fontSize: "50px", marginBottom: "30px" }}
+        >
+          ورود
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="text-white h1 text-end mx-4 my-2 fw-bold">
+            <div className="login-div">
+              <div className="login-email-div">
+                <input
+                  name="username"
+                  type="text"
+                  className="form-control login-input"
+                  id="floatingInput"
+                  placeholder="نام کاربری"
+                  onChange={handleUsernameChange}
+                />
+              </div>
+              <hr className="border-bottom-input-login"></hr>
+              <div className="login-error">{username}</div>
+              <div className="login-password-div" style={{ display: "flex" }}>
+                <input
+                  name="password"
+                  type="password"
+                  className="form-control login-input"
+                  id="floatingPassword"
+                  placeholder="رمز عبور"
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              <hr className="border-bottom-input-login"></hr>
+
+              <div className="login-error">{password}</div>
+
+              <div className="login-button">
+                <a
+                  href={process.env.PUBLIC_URL}
+                  className="btn btn-primary fw-bold forget-password-button"
+                >
+                  بازیابی رمز عبور
+                </a>
+                <button className="btn btn-primary fw-bold">ورود</button>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        <div className="d-flex justify-content-evenly">
+          <hr className="left-line-login-by"></hr>
+          <span className="text-white mt-1">ورود با</span>
+          <hr className="right-line-login-by"></hr>
+        </div>
+
+        <div className="mx-4 google-linkedin-button mt-4">
+          <a className="signin-linkedin-a" href={process.env.PUBLIC_URL}>
+            <span className="">لینکدین</span>
+            <img
+              src={
+                process.env.PUBLIC_URL + "/picturs/auth/login/linkedin_logo.png"
+              }
+              alt=""
+              className="signin-linkedin-img"
+            />
+          </a>
+          <a className="signin-google-a" href={process.env.PUBLIC_URL}>
+            <span className="">گوگل</span>
+            <img
+              src={
+                process.env.PUBLIC_URL + "/picturs/auth/login/google_logo.png"
+              }
+              alt=""
+              className="signin-google-img"
+            />
+          </a>
+        </div>
+
+        <div className="register-div justify-content-end align-items-baseline mx-4">
+          <Link
+            to="/signup"
+            className="btn btn-prim fw-bold py-2 register-button m-0"
+          >
+            ثبت نام کنید
+          </Link>
+          <p className="fw-bold justify-content-space-between">
+            آیا ثبت نام نکرده‌اید؟!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/authentication/signup.js
@@ -0,0 +1,274 @@
+import { useState, useEffect } from "react";
+import "../../styles/authentication/signup.css";
+import { Link } from "react-router-dom";
+import { api } from "../../vars/JwtToken.js";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
+function Signup() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [passerror, setPasserror] = useState("");
+  const [confirmPasserror, setConfirmPasserror] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("jwtAccessToken")) {
+      navigate("/");
+    }
+  }, []);
+
+  const handleFirstnameChange = (event) => {
+    setFirstname("");
+  };
+  const handleLastnameChange = (event) => {
+    setLastname("");
+  };
+  const handleEmailChange = (event) => {
+    setEmail("");
+  };
+  const handlePasswordChange = (event) => {
+    setPasserror("");
+  };
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPasserror("");
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const firstname = e.target.elements.firstname.value;
+    const lastname = e.target.elements.lastname.value;
+    const email = e.target.elements.email.value;
+    const password = e.target.elements.password.value;
+    const confirmpassword = e.target.elements.confirmpassword.value;
+
+    const registerUser = async (firstname, lastname, email, password, confirmpassword) => {
+      var data = {
+        email: email,
+        // first_name: firstname,
+        // last_name: lastname,
+        username: firstname + lastname,
+        password: password,
+      };
+
+      console.log(data);
+
+      if (password !== confirmpassword) {
+        setConfirmPasserror("تکرار رمز عبور با رمز عبور یکسان نیست");
+        return;
+      }
+      try {
+        const response = await api.post("/auth/users/", data);
+        console.log(response);
+        if (response.status === 201) {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
+
+          Toast.fire({
+            icon: "success",
+            title: "ثبت نام با موفقیت انجام شد",
+          });
+          navigate("/login");
+        }
+      } catch (error) {
+        console.log(error);
+        if (error) {
+          if (error.response.data.email) {
+            if (
+              error.response.data.email[0] ===
+              "user with this email already exists."
+            ) {
+              setEmail("کاربر با این ایمیل قبلا ثبت نام کرده است");
+            } else if (
+              error.response.data.email[0] === "Enter a valid email address."
+            ) {
+              setEmail("یک ایمیل معتبر وارد نمایید");
+            } else if (
+              error.response.data.email[0] === "This field may not be blank."
+            ) {
+              setEmail("فیلد ایمیل الزامی است");
+            }
+          }
+          if (error.response.data.password) {
+            if (
+              error.response.data.password[0] ===
+              "The password is too similar to the first name."
+            ) {
+              setPasserror("رمز عبور بسیار شبیه به نام می باشد");
+            } else if (
+              error.response.data.password[0] ===
+              "The password is too similar to the username."
+            ) {
+              setPasserror("رمز عبور بسیار شبیه به نام یا نام خانوادگی می باشد");
+            } else if (
+              error.response.data.password[0] ===
+              "The password is too similar to the email."
+            ) {
+              setPasserror("رمز عبور بسیار شبیه به ایمیل می باشد");
+            } else if (
+              error.response.data.password[0] ===
+              "This password is too short. It must contain at least 8 characters."
+            ) {
+              setPasserror("رمز عبور باید بیشتر از 8 کاراکتر باشد");
+            } else if (
+              error.response.data.password[0] ===
+              "This password is entirely numeric."
+            ) {
+              setPasserror(
+                "رمز عبور باید حداقل یک کاراکتر غیر عددی داشته باشد"
+              );
+            } else if (
+              error.response.data.password[0] === "This field may not be blank."
+            ) {
+              setPasserror("فیلد رمز عبور الزامی است");
+            }
+          }
+
+          return;
+        }
+      }
+    };
+
+    registerUser(firstname, lastname, email, password, confirmpassword);
+  };
+
+  return (
+    <div className="signup-container">
+      <img
+        className="signup-background-img"
+        src={process.env.PUBLIC_URL + "/picturs/auth/signup/background.png"}
+        alt=""
+        style={{ position: "absolute", top: "0px" }}
+      />
+      <div className="signup-box py-3">
+        <div
+          className="text-white h1 text-end mx-4"
+          style={{ fontSize: "50px", marginBottom: "10px" }}
+        >
+          ثبت نام
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="text-white h1 text-end mx-4 my-2 fw-bold">
+            <div className="signup-div">
+              <div className="d-flex">
+                <div>
+                  <div className="signup-error">{firstname}</div>
+                  <div className="signup-lastname-div">
+                    <input
+                      name="lastname"
+                      type="text"
+                      className="form-control signup-input"
+                      placeholder="نام خانوادگی"
+                      onChange={handleLastnameChange}
+                    />
+                  </div>
+                  <hr className="border-bottom-input-signup"></hr>
+                </div>
+                <div>
+                  <div className="signup-firstname-div">
+                    <input
+                      name="firstname"
+                      type="text"
+                      className="form-control signup-input"
+                      placeholder="نام"
+                      onChange={handleFirstnameChange}
+                    />
+                  </div>
+                  <hr className="border-bottom-input-signup"></hr>
+                </div>
+              </div>
+              <div className="signup-error">{lastname}</div>
+              <div className="signup-email-div">
+                <input
+                  name="email"
+                  type="email"
+                  className="form-control signup-input"
+                  placeholder="آدرس ایمیل"
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <hr className="border-bottom-input-signup"></hr>
+              <div className="signup-error">{email}</div>
+              <div className="signup-password-div" style={{ display: "flex" }}>
+                <input
+                  name="password"
+                  type="password"
+                  className="form-control signup-input"
+                  placeholder="رمز عبور"
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              <hr className="border-bottom-input-signup"></hr>
+              <div className="signup-error">{passerror}</div>
+              <div className="signup-password-div" style={{ display: "flex" }}>
+                <input
+                  name="confirmpassword"
+                  type="password"
+                  className="form-control signup-input"
+                  placeholder="تکرار رمز عبور"
+                  onChange={handleConfirmPasswordChange}
+                />
+              </div>
+              <hr className="border-bottom-input-signup"></hr>
+              <div className="signup-error">{confirmPasserror}</div>
+              <div className="signup-button">
+                <button className="btn btn-primary fw-bold mt-1">تایید</button>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        <div className="d-flex justify-content-evenly">
+          <hr className="left-line-signup-by"></hr>
+          <span className="text-white mt-1">ثبت نام با</span>
+          <hr className="right-line-signup-by"></hr>
+        </div>
+
+        <div className="mx-4 google-linkedin-button mt-4">
+          <a className="signin-linkedin-a" href={process.env.PUBLIC_URL}>
+            <span className="">لینکدین</span>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/picturs/auth/signup/linkedin_logo.png"
+              }
+              alt=""
+              className="signin-linkedin-img"
+            />
+          </a>
+          <a className="signin-google-a" href={process.env.PUBLIC_URL}>
+            <span className="">گوگل</span>
+            <img
+              src={
+                process.env.PUBLIC_URL + "/picturs/auth/signup/google_logo.png"
+              }
+              alt=""
+              className="signin-google-img"
+            />
+          </a>
+        </div>
+
+        <div className="signup-div text-center">
+          <Link
+            to="/login"
+            className="btn btn-prim fw-bold py-2 register-button"
+          >
+            قبلا ثبت نام کرده ام
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
